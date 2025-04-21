883cbde33d079b223dbe4c4612f54be469f58e3f
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2025-04-21 21:48:21 +0100

    return validation status service

--- app/services/validation_service.py
+++ app/services/validation_service.py
@@ -8,7 +8,10 @@ import logging
 
 from flask import jsonify, Response
 
-from app.tasks.validation_tasks import process_validation_task_by_id
+from app.tasks.validation_tasks import (
+    process_validation_task_by_id,
+    return_ro_crate_validation
+    )
 
 logger = logging.getLogger(__name__)
 
@@ -37,3 +40,24 @@ def queue_ro_crate_validation_task(
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
+def get_ro_crate_validation_task(
+    crate_id
+) -> tuple[Response, int]:
+    """
+    Retrieves an RO-Crate validation result.
+
+    :param crate_id: The ID of the RO-Crate to validate.
+    :return: A tuple containing a JSON response and an HTTP status code.
+    :raises Exception: If an error occurs whilst retrieving RO-Crate
+    """
+    logging.info(f"Retrieving validation for: {crate_id}")
+
+    if not crate_id:
+        return jsonify({"error": "Missing required parameter: crate_id"}), 400
+
+    try:
+        return return_ro_crate_validation(crate_id), 200
+    except Exception as e:
+        return jsonify({"service error": str(e)}), 500
