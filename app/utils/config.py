01766230695ffe62579b5e2a3264d81a6da46109
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2025-07-20 21:25:47 +0100

    create InvalidAPIUsage class

--- app/utils/config.py
+++ app/utils/config.py
@@ -40,6 +40,22 @@ class ProductionConfig(Config):
     ENV = "production"
 
 
+class InvalidAPIUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        super().__init__()
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+
 def make_celery(app: Flask = None) -> Celery:
     """
     Initialises and configures a Celery instance with the Flask application.
