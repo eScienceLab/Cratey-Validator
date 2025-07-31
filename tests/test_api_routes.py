ac4187eb2e742035c0a9a9c8f0cc4174ab53363c
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2025-07-31 22:13:10 +0100

    validate_metadata api tests parameterised

--- tests/test_api_routes.py
+++ tests/test_api_routes.py
@@ -25,13 +25,36 @@ def client():
             ),
             (
                 "crate-123", {
+                    "minio_bucket": "test_bucket",
+                    "root_path": "base_path",
+                    "webhook_url": "https://webhook.example.com",
+                }, 202, {"message": "Validation in progress"}
+            ),
+            (
+                "crate-123", {
+                    "minio_bucket": "test_bucket",
+                    "root_path": "base_path",
+                    "profile_name": "default"
+                }, 202, {"message": "Validation in progress"}
+            ),
+            (
+                "crate-123", {
+                    "minio_bucket": "test_bucket",
+                    "webhook_url": "https://webhook.example.com",
+                    "profile_name": "default"
+                }, 202, {"message": "Validation in progress"}
+            ),
+            (
+                "crate-123", {
                     "minio_bucket": "test_bucket"
                 }, 202, {"message": "Validation in progress"}
             ),
         ],
-        ids=["validate_by_id", "validate_with_missing_root_path_and_profile_name_and_webhook_url"]
+        ids=["validate_by_id", "validate_with_missing_profile_name",
+             "validate_with_missing_webhook_url", "validate_with_missing_root_path",
+             "validate_with_missing_root_path_and_profile_name_and_webhook_url"]
 )
-def test_validate_by_id_success(client, crate_id, payload, status_code, response_json):
+def test_validate_by_id_success(client: FlaskClient, crate_id: str, payload: dict, status_code: int, response_json: dict):
     with patch("app.ro_crates.routes.post_routes.queue_ro_crate_validation_task") as mock_queue:
         mock_queue.return_value = (response_json, status_code)
 
@@ -70,175 +93,75 @@ def test_validate_by_id_success(client, crate_id, payload, status_code, response
         "missing_minio_bucket_returns_422"
     ]
 )
-def test_validate_fails_missing_elements(client, crate_id, payload, status_code):
+def test_validate_fails_missing_elements(client: FlaskClient, crate_id: str, payload: dict, status_code: int):
     response = client.post(f"/v1/ro_crates/{crate_id}/validation", json=payload)
     assert response.status_code == status_code
 
 
-
-
-def test_validate_by_id_missing_profile_name(client):
-    crate_id = "crate-123"
-    payload = {
-        "minio_bucket": "test_bucket",
-        "root_path": "base_path",
-        "webhook_url": "https://webhook.example.com"
-    }
-
-    with patch("app.ro_crates.routes.post_routes.queue_ro_crate_validation_task") as mock_queue:
-        mock_queue.return_value = ({"message": "Validation in progress"}, 202)
-
-        response = client.post(f"/v1/ro_crates/{crate_id}/validation", json=payload)
-
-        assert response.status_code == 202
-        assert response.json == {"message": "Validation in progress"}
-        mock_queue.assert_called_once_with("test_bucket", "crate-123", "base_path", None, "https://webhook.example.com")
-
-
-def test_validate_by_id_missing_webhook_url(client):
-    crate_id = "crate-123"
-    payload = {
-        "minio_bucket": "test_bucket",
-        "root_path": "base_path",
-        "profile_name": "default"
-    }
-
-    with patch("app.ro_crates.routes.post_routes.queue_ro_crate_validation_task") as mock_queue:
-        mock_queue.return_value = ({"message": "Validation in progress"}, 202)
-
-        response = client.post(f"/v1/ro_crates/{crate_id}/validation", json=payload)
-
-        assert response.status_code == 202
-        assert response.json == {"message": "Validation in progress"}
-        mock_queue.assert_called_once_with("test_bucket", "crate-123", "base_path", "default", None)
-
-
-def test_validate_by_id_missing_root_path(client):
-    crate_id = "crate-123"
-    payload = {
-        "minio_bucket": "test_bucket",
-        "profile_name": "default",
-        "webhook_url": "https://webhook.example.com"
-    }
-
-    with patch("app.ro_crates.routes.post_routes.queue_ro_crate_validation_task") as mock_queue:
-        mock_queue.return_value = ({"message": "Validation in progress"}, 202)
-
-        response = client.post(f"/v1/ro_crates/{crate_id}/validation", json=payload)
-
-        assert response.status_code == 202
-        assert response.json == {"message": "Validation in progress"}
-        mock_queue.assert_called_once_with("test_bucket", "crate-123", None, "default", "https://webhook.example.com")
-
-
 # Test POST API: /v1/ro_crates/validate_metadata
 
-def test_validate_metadata_with_all_fields(client: FlaskClient):
-    """
-    If both profile name and crate json are provided then processing
-    of the RO-Crate metadata should continue, and return:
-        - 200 status code
-        - JSON data object which includes "status" which is "success"
-    """
-    test_data = {
-        "crate_json": '{"@context": "https://w3id.org/ro/crate/1.1/context"}',
-        "profile_name": "default"
-    }
-
+@pytest.mark.parametrize(
+    "payload, status_code, response_json",
+    [
+        (
+            {
+                "crate_json": '{"@context": "https://w3id.org/ro/crate/1.1/context"}',
+                "profile_name": "default"
+            }, 200, {"status": "success"}
+        ),
+        (
+            {
+                "crate_json": '{"@context": "https://w3id.org/ro/crate/1.1/context"}',
+            }, 200, {"status": "success"}
+        ),
+    ],
+    ids=["success_with_all_fields", "success_without_profile_name"]
+)
+def test_validate_metadata_success(client: FlaskClient, payload: dict, status_code: int, response_json: dict):
     with patch("app.ro_crates.routes.post_routes.queue_ro_crate_metadata_validation_task") as mock_queue:
-        mock_queue.return_value = ({"status": "success"}, 200)
+        mock_queue.return_value = (response_json, status_code)
 
-        response = client.post("/v1/ro_crates/validate_metadata", json=test_data)
-        print(response.json)
-        mock_queue.assert_called_once_with(
-                test_data["crate_json"],
-                test_data["profile_name"]
-                )
-        assert response.status_code == 200
-        assert response.json == {"status": "success"}
+        response = client.post("/v1/ro_crates/validate_metadata", json=payload)
 
+        crate_json = payload["crate_json"] if "crate_json" in payload else None
+        profile_name = payload["profile_name"] if "profile_name" in payload else None
 
-def test_validate_metadata_without_profile_name(client: FlaskClient):
-    """
-    If the profile name is not specified then processing
-    of the RO-Crate metadata should continue, and return:
-        - 200 status code
-        - JSON data object which includes "status" which is "success"
-    """
-    test_data = {
-        "crate_json": '{"@context": "https://w3id.org/ro/crate/1.1/context"}'
-    }
-
-    with patch("app.ro_crates.routes.post_routes.queue_ro_crate_metadata_validation_task") as mock_queue:
-        mock_queue.return_value = ({"status": "success"}, 200)
-
-        response = client.post("/v1/ro_crates/validate_metadata", json=test_data)
-        mock_queue.assert_called_once_with(test_data["crate_json"], None)
-        assert response.status_code == 200
-        assert response.json == {"status": "success"}
+        mock_queue.assert_called_once_with(crate_json, profile_name)
+        assert response.status_code == status_code
+        assert response.json == response_json
 
 
-def test_validate_metadata_missing_crate_json(client: FlaskClient):
-    """
-    If the RO-Crate is missing APIFlask should return:
-        - 422 status code
-        - Error message which includes 'Missing data for required field'
-    """
-    test_data = {
-        "profile_name": "default"
-    }
-
-    response = client.post("/v1/ro_crates/validate_metadata", json=test_data)
-    assert response.status_code == 422
-    assert "Missing data for required field" in response.get_data(as_text=True)
-
-
-def test_validate_metadata_emptystring_crate_json(client: FlaskClient):
-    """
-    If the RO-Crate is missing APIFlask should return:
-        - 422 status code
-        - Error message which includes 'Missing required parameter'
-    """
-    test_data = {
-        "crate_json": "",
-        "profile_name": "default"
-    }
-
-    response = client.post("/v1/ro_crates/validate_metadata", json=test_data)
-    assert response.status_code == 422
-    assert "Missing required parameter" in response.get_data(as_text=True)
-
-
-def test_validate_metadata_malformed_crate_json(client: FlaskClient):
-    """
-    If the RO-Crate is missing APIFlask should return:
-        - 422 status code
-        - Error message which includes 'not valid JSON'
-    """
-    test_data = {
-        "crate_json": "{",
-        "profile_name": "default"
-    }
-
-    response = client.post("/v1/ro_crates/validate_metadata", json=test_data)
-    assert response.status_code == 422
-    assert "not valid JSON" in response.get_data(as_text=True)
-
-
-def test_validate_metadata_emptydict_crate_json(client: FlaskClient):
-    """
-    If the RO-Crate is missing APIFlask should return:
-        - 422 status code
-        - Error message which includes 'Required parameter crate_json is empty'
-    """
-    test_data = {
-        "crate_json": "{}",
-        "profile_name": "default"
-    }
-
-    response = client.post("/v1/ro_crates/validate_metadata", json=test_data)
-    assert response.status_code == 422
-    assert "Required parameter crate_json is empty" in response.get_data(as_text=True)
+@pytest.mark.parametrize(
+    "payload, status_code, response_text",
+    [
+        (
+            {
+                "profile_name": "default"
+            }, 422, "Missing data for required field"
+        ),
+        (
+            {
+                "crate_json": '',
+            }, 422, "Missing required parameter"
+        ),
+        (
+            {
+                "crate_json": '{',
+            }, 422, "not valid JSON"
+        ),
+        (
+            {
+                "crate_json": '{}',
+            }, 422, "Required parameter crate_json is empty"
+        ),
+    ],
+    ids=["failure_missing_crate", "failure_empty_crate",
+         "failure_malformed_crate", "failure_empty_crate"]
+)
+def test_validate_metadata_failure(client: FlaskClient, payload: dict, status_code: int, response_text: str):
+    response = client.post("/v1/ro_crates/validate_metadata", json=payload)
+    assert response.status_code == status_code
+    assert response_text in response.get_data(as_text=True)
 
 
 # Test GET API: /v1/ro_crates/{crate_id}/validation
